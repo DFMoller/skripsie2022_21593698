1334e3b29dac754043b1777bbcd97da164ffd6aa
Daniel Francois Moller <21593698@sun.ac.za>
2022-08-15 23:14:12 +0200

    Initial Quotation Class definition

---
+++ website/quotation.py
@@ -0,0 +1,14 @@
+
+
+class Quotation:
+    battery_voltage = 24
+    def __init__(self, powerHours, chargingHours):
+        self.power_hours = powerHours
+        self.charging_hours = chargingHours
+    def generate_minimum_parameters(self):
+        pass
+    def calculate_cost(self):
+        pass
+    def display_results(self):
+        pass
+    
